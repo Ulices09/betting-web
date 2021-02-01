ee636d3076a60ac2bc1a10af1b159d3a79e273ed
Ulices Meléndez Acosta <ulices009@hotmail.com>
2021-02-01 01:22:03 -0500

    Add event list tests

---
+++ src/scenes/eventList/EventList.test.tsx
@@ -0,0 +1,93 @@
+import { shallow } from 'enzyme';
+import EventList from './EventList.view';
+
+jest.mock('hooks', () => {
+  return {
+    useRequest: () => ({
+      loading: false,
+      error: null,
+      data: [
+        {
+          id: 'EVT_1',
+          name: 'Real Madrid vs Barcelona',
+          markets: [
+            {
+              id: 'MKT_1',
+              name: 'Team to Win',
+              selections: [
+                {
+                  id: 'SEL_1',
+                  name: 'Real Madrid',
+                  price: 1.23,
+                },
+                {
+                  id: 'SEL_2',
+                  name: 'Barcelona',
+                  price: 2.05,
+                },
+              ],
+            },
+            {
+              id: 'MKT_2',
+              name: 'Player to Score First',
+              selections: [
+                {
+                  id: 'SEL_3',
+                  name: 'Ronaldo',
+                  price: 1.15,
+                },
+                {
+                  id: 'SEL_4',
+                  name: 'Messi',
+                  price: 1.3,
+                },
+                {
+                  id: 'SEL_5',
+                  name: 'Bale',
+                  price: 1.35,
+                },
+              ],
+            },
+          ],
+        },
+        {
+          id: 'EVT_2',
+          name: 'Atletico Madrid vs Malaga',
+          markets: [
+            {
+              id: 'MKT_3',
+              name: 'Team to Win',
+              selections: [
+                {
+                  id: 'SEL_6',
+                  name: 'Atletico',
+                  price: 1.4,
+                },
+                {
+                  id: 'SEL_7',
+                  name: 'Malaga',
+                  price: 3.05,
+                },
+              ],
+            },
+          ],
+        },
+        {
+          id: 'EVT_3',
+          name: "Empty Event that shouldn't render",
+          markets: [],
+        },
+      ],
+    }),
+  };
+});
+
+test('renders', () => {
+  const wrapper = shallow(<EventList />);
+  expect(wrapper.exists()).toBe(true);
+});
+
+test('renders items', () => {
+  const wrapper = shallow(<EventList />);
+  expect(wrapper.find('EventItem').length).toBe(2);
+});
