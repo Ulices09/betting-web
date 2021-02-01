b0a4b5e54490e57995b323d1cdfb0b0f0ac553bd
Ulices Meléndez Acosta <ulices009@hotmail.com>
2021-02-01 00:24:44 -0500

    Add Drawer tests

---
+++ src/components/drawer/Drawer.test.tsx
@@ -0,0 +1,19 @@
+import { shallow } from 'enzyme';
+import Drawer from './Drawer.view';
+
+describe('Drawer component', () => {
+  test('renders', () => {
+    const wrapper = shallow(<Drawer isOpen={false} onClickClose={() => {}} />);
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  test('calls onClickClose', () => {
+    const onClickClose = jest.fn();
+    const wrapper = shallow(
+      <Drawer isOpen={true} onClickClose={onClickClose} />,
+    );
+    expect(onClickClose).not.toBeCalled();
+    wrapper.find('img').simulate('click');
+    expect(onClickClose).toBeCalled();
+  });
+});
